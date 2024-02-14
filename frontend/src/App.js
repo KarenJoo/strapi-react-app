b6b9f9269a3ac9dab4fa5141a87a4adb8cd05293
Karen Jo <karenfiksdahl@gmail.com>
2024-02-14 18:13:26 +0100

    updates router to include router and switch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,23 +5,26 @@ import ProductDetails from './pages/ProductDetails';
 import Category from './pages/Category';
 import SiteHeader from './components/SiteHeader';
 
+
 function App() {
   return (
-    <div className="App">
-      <SiteHeader/ >
-      <Switch>
-        <Route path="/">
-          <Homepage />
-        </Route>
-        <Route path="/details/:id">
-          <ProductDetails />
-        </Route>
-        <Route path="/category/:id">
-          <Category />
-        </Route>
-      </Switch>
-      <h1>ShoeDays</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <SiteHeader />
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route exact path="/details/:id">
+            <ProductDetails />
+          </Route>
+          <Route exact path="/category/:id">
+            <Category />
+          </Route>
+        </Switch>
+        <h1>ShoeDays</h1>
+      </div>
+    </Router>
   );
 }
 
