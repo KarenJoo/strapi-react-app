19e468e3a0e0864903ec030d1c0c00ae7022ba4b
Karen Jo <karenfiksdahl@gmail.com>
2024-02-14 17:51:53 +0100

    installs react-router-dom, adds switch components to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Homepage from './pages/Homepage';
+import ProductDetails from './pages/ProductDetails';
+import Category from './pages/Category';
+import SiteHeader from './components/SiteHeader';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <SiteHeader/ >
+      <Switch>
+        <Route path="/">
+          <Homepage />
+        </Route>
+        <Route path="/details/:id">
+          <ProductDetails />
+        </Route>
+        <Route path="/category/:id">
+          <Category />
+        </Route>
+      </Switch>
+      <h1>ShoeDays</h1>
     </div>
   );
 }
