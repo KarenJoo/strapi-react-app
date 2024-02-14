d28d77992b17275aa30a6354c5b17d888bab2dc0
Karen Jo <karenfiksdahl@gmail.com>
2024-02-14 16:57:03 +0100

    adds .prettierrc

---
+++ frontend/src/components/SiteHeader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SiteHeader() {
+  return (
+    <div className="site-header">
+        <Link to="/"><h1>ShoeDays</h1></Link>
+        </div>
+  )
+}
