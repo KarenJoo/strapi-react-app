26dee09ad0b12cc8eda48f13cea37eed5271e4fd
Karen Jo <karenfiksdahl@gmail.com>
2024-02-17 14:01:29 +0100

    adjusts img to img container, product list wraps all products

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -3,25 +3,22 @@ import useFetch from '../hooks/useFetch';
 import { Link } from 'react-router-dom';
 
 function ProductCard({ product }) {
-    return (
-      <div className="product-card">
-         <h2>{product.attributes.name}</h2>
-         <div className="image-container">
+  return (
+    <div className="product-card">
+      <h2>{product.attributes.name}</h2>
+      <div className="image-container">
         <img src={`http://localhost:1337${product.attributes.image.data.attributes.url}`} alt={product.attributes.name} />      
-       </div>
-       <div className="card-content">
+      </div>
+      <div className="card-content">
         <p>{product.attributes.description}</p>
         <Link to={`/products/${product.id}`}><p>Read more</p></Link>
         <h5><p>Price:</p>{product.attributes.price}</h5>
-        </div>
       </div>
-    );
-  }
-  
-  
+    </div>
+  );
+}
 
 function Homepage() {
-  // Construct the URL with the populate parameter
   const { loading, error, data } = useFetch('http://localhost:1337/api/products?populate=*');
   
   if (loading) {
@@ -32,9 +29,8 @@ function Homepage() {
     return <p>Error occurred!</p>;
   }
   
-  console.log(data)
   return (
-    <div>
+    <div className="product-list">
       {data.map(product => (
         <ProductCard key={product.id} product={product} />
       ))}
